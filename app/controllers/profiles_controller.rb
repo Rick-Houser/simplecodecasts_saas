4479bddcd6a3cc0fad001c9443652f20984bf374
Rick Houser <frakentoaster@gmail.com>
2015-06-25 01:35:42 +0000

    Home page improvements

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   def new
     # form for each user to fill out their own profile
     @user = User.find( params[:user_id] )
-    @profile = @user.build_profile
+    @profile = Profile.new
   end
   
   def create
